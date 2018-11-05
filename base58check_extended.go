3898db9a6bad9ff45d83cda3e6dd45d90e6afe51
sammy00 <xiangminli@outlook.com>
2018-11-05 16:39:01 +0800

    add a extended encode/decode

---
+++ base58check_extended.go
@@ -0,0 +1,36 @@
+package base58
+
+import (
+	"bytes"
+)
+
+// CheckDecodeX extends CheckDecode by allowing more version bytes
+func CheckDecodeX(input string, versionLen int) ([]byte, []byte, error) {
+	decoded := Decode(input)
+
+	if len(decoded) < ChecksumLen+versionLen {
+		return nil, nil, ErrInvalidFormat
+	}
+
+	if cksum := Checksum(decoded[:len(decoded)-4]); !bytes.Equal(
+		cksum[:], decoded[len(decoded)-4:]) {
+		return nil, nil, ErrChecksum
+	}
+
+	version := decoded[:versionLen]
+	payload := decoded[versionLen : len(decoded)-4]
+
+	return version, payload, nil
+}
+
+// CheckEncodeX extends CheckEncode by allowing more version bytes
+func CheckEncodeX(version, input []byte) string {
+	b := make([]byte, 0, len(version)+len(input)+4)
+	b = append(b, version...)
+	b = append(b, input...)
+
+	cksum := Checksum(b)
+	b = append(b, cksum[:]...)
+
+	return Encode(b)
+}
---
+++ constant.go
@@ -0,0 +1,4 @@
+package base58
+
+// ChecksumLen is the length of checksum appended to the data to encode
+const ChecksumLen = 4
